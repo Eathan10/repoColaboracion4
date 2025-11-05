e10b621e99ab716d5aab688bbd992bb152fff46e
Eathan <eathanandres.garzon.51296@ikasle.egibide.org>
2025-11-05 12:06:25 +0100

    Funcion del alimno 1 creada

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,8 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+
+    public static void funcionAlumno1() {
+        System.out.println("Función desarrollada por Alumno 1");
+    }
 }
