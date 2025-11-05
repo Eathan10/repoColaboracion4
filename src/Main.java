19e550f181a4a83f5cffbda120d37ed4aaf01400
Zaira <zaira.tatay.52980@ikasle.egibide.org>
2025-11-05 12:07:51 +0100

    Zaira:Funcion de alumno 2 creada

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,7 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+    public static void funcionZaira(){
+        System.out.println("Función desarrollada por Zaira");
+    }
 }
