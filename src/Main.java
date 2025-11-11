94ec0f0a63603f1a5854f750ec245f35be41a948
Unax.L <unax.gonzalezdelangarica.51316@ikasle.egibide.org>
2025-11-11 22:14:53 +0100

    Commit Unax

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        public class Main {
+            public static void main(String[] args) {
+                System.out.println(sumar(3, 5));
+            }
+
+            static int sumar(int a, int b) {
+                return a + b;
+            }
+        }
+        ;
+    }
+}
